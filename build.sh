62beafe1fd86c54e9b843b87c42b0ea630380c2f
孟古一 <my@mengguyi.com>
2023-01-03 19:56:38 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ else
     git clone https://ghproxy.com/https://github.com/RimoChan/sese-engine $SESE_DIR
     cd $SESE_DIR && cat requirements.txt | grep -v orjson | grep -v guppy3 > new_requirements.txt && echo orjson >> new_requirements.txt
     mv new_requirements.txt requirements.txt
+    sed 's/loli_spider/Googlebot/g' 配置.py
     cd ..
 fi
 docker build . -t mengguyi/sese-engine
